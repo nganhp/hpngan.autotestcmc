b742833f9197cba39ad15cd0d36c9d9257b343fa
nganhp <nganhp21@gmail.com>
2025-03-18 22:59:44 +0700

    updated

---
+++ Lesson5/RobotHutBui.java
@@ -0,0 +1,32 @@
+package com.cmcglobal.Lesson5;
+
+public class RobotHutBui {
+    private String model;
+    private int trongLuong;
+    private int theTich;
+    private int luongNuoc;
+
+public RobotHutBui(String model, int theTich) {
+    this.model = model;
+    this.theTich = theTich;
+    this.trongLuong = theTich * 2 + 100;
+    this.luongNuoc = 0;
+}
+public int getLuongNuoc(){
+    return this.luongNuoc;
+}
+public int getTrongLuong(){
+    return this.trongLuong;
+}
+
+public void layNuoc (){
+    this.luongNuoc = theTich;
+}
+public int chamNuoc(){
+   int nuocDoRa = this.luongNuoc;
+   this.luongNuoc = 0;
+   return nuocDoRa;
+
+}
+
+}
